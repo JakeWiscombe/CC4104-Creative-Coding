08214a9bc2266d583049650ac2511cf94d62bd45
JakeWiscombe <JAKE.WISCOMBE24@bathspa.ac.uk>
2025-06-02 02:24:50 +0100

    Updated ExperimentOne Files

--- Experiment1-VisualTimeAlternative/sketch.js
+++ Experiment1-VisualTimeAlternative/sketch.js
@@ -1,4 +1,8 @@
-// Global va
+// Global variables
+let hr, min, sec;
+let hourWidth, minuteWidth, secondWidth;
+let barHeight = 50;
+let spacing = 20;
 
 function setup() {
   createCanvas(500, 300);
@@ -11,34 +15,32 @@ function draw() {
   background(30);  // Dark background
 
   // Get current time
-  let hr = hour() % 24; // sets to 24hr clock
-  let min = minute(); // minutes
-  let sec = second(); // seconds
-
-  // Map time units to bar width
-  let hourWidth = map(hr + min / 60, 0, 24, 0, width); // 
-  let minuteWidth = map(min + sec / 60, 0, 60, 0, width);
-  let secondWidth = map(sec, 0, 60, 0, width);
-
-  // Bar height and spacing
-  let barHeight = 50;
-  let spacing = 20;
+  hr = hour() % 24;
+  min = minute();
+  sec = second();
+  
+  //value start stop start stop
+  
+  // Time units that moves the bar per sec, min + hour
+  hourWidth = map(hr + min / 60, 0, 24, 0, width); // 15 + 30 / 60
+  minuteWidth = map(min + sec / 60, 0, 60, 0, width);
+  secondWidth = map(sec, 0, 60, 0, width);
 
   // Draw hour bar
-  fill(70, 130, 180);  // Steel blue
-  rect(0, spacing, hourWidth, barHeight);
-  fill(255);
+  fill(70, 130, 180); // Colour for moving bar
+  rect(0, spacing, hourWidth, barHeight, 0, 10, 10 ,0);
+  fill(255); // Colour for text and number
   text("Hours: " + nf(hr, 2), 10, spacing + barHeight / 2);
 
   // Draw minute bar
-  fill(60, 179, 113);  // Medium sea green
-  rect(0, spacing * 2 + barHeight, minuteWidth, barHeight);
-  fill(255);
+  fill(60, 179, 113); // Colour for moving bar
+  rect(0, spacing * 2 + barHeight, minuteWidth, barHeight, 0, 10, 10 ,0); // Draw bar
+  fill(255); // Colour for text and number
   text("Minutes: " + nf(min, 2), 10, spacing * 2 + barHeight * 1.5);
 
   // Draw second bar
-  fill(220, 20, 60);  // Crimson
-  rect(0, spacing * 3 + barHeight * 2, secondWidth, barHeight);
-  fill(255);
+  fill(220, 20, 60); // Colour for moving bar
+  rect(0, spacing * 3 + barHeight * 2, secondWidth, barHeight, 0, 10, 10 ,0);
+  fill(255); // Colour for text and number
   text("Seconds: " + nf(sec, 2), 10, spacing * 3 + barHeight * 2.5);
 }
